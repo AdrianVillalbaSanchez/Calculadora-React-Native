bea841c3528006be6f3ecb3e8b8bee2fcba005b5
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-09-18 20:38:02 +0200

    Creating Calc component

---
+++ calculadora/src/components/screens/Calc.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Calc extends Component {
+
+    render() {
+        return (
+            <View><Text> Hola</Text> </View>
+        )
+    }
+}
+
+export default Calc;
