2d0ca9ad31a1c38da4f858cb308ce4fed0fadf9c
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-09-18 23:39:10 +0200

    Now TextInput has CSS

--- calculadora/src/components/screens/Calc.js
+++ calculadora/src/components/screens/Calc.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 
 class Calc extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: "red" }}>
-                <Text> Hola </Text>
+                <TextInput style={{
+                    styles.input
+                }} />
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: 'rgb(41, 41, 41)',
+        height: 150,
+        color: 'rgb(255,255,255)',
+        fontSize: 48,
+        textAlign: 'right'
+    }
+})
 
 export default Calc;
