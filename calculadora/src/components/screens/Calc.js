abca194026302b66c51c13286526ad72f30a9e1c
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-09-19 18:54:36 +0200

    Signs added

--- calculadora/src/components/screens/Calc.js
+++ calculadora/src/components/screens/Calc.js
@@ -10,7 +10,8 @@ class Calc extends React.Component {
 
         //class variable, numeros que se usan
         this.validKeys = [
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "+", "-", "/", "*", "=" // son strings ya que el textinput solo acepta strings.
         ]
     }
 
