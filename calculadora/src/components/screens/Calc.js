2cfd6b351c83e60143ff560d0bfc33eaf0c35e12
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-09-19 18:57:37 +0200

    Some comment added

--- calculadora/src/components/screens/Calc.js
+++ calculadora/src/components/screens/Calc.js
@@ -13,6 +13,9 @@ class Calc extends React.Component {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "+", "-", "/", "*", "=" // son strings ya que el textinput solo acepta strings.
         ]
+
+        // Estos "numeros" y "signos" tenemos que convertirlos a números de verdad, 
+        // solo aceptar la introducción de estos.
     }
 
     handleInput(text) {
