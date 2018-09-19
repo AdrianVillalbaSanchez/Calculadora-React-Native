2d7b00adba8f3946bf507bc1ae359029303ad132
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-09-19 18:48:50 +0200

    array's numbers (0 to 9) added

--- calculadora/src/components/screens/Calc.js
+++ calculadora/src/components/screens/Calc.js
@@ -6,7 +6,12 @@ class Calc extends React.Component {
         super()
         this.state = {
             inputText: "" // "no queremos controlar el valor sino mantenerlo"
-        }
+        };
+
+        //class variable, numeros que se usan
+        this.validKeys = [
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        ]
     }
 
     handleInput(text) {
